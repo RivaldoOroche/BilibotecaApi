6c96988aa7c3350d6541b99ad4fb4ebf958e71d4
RivaldoOroche <rivlado99@gmail.com>
2023-01-18 23:28:08 -0500

    Agregando, ApiRoutes y el controlador de cliente

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Biblioteca.Cliente.Api.Controllers
+{
+    public class ClienteController
+    {
+    }
+}
---
+++ Routes/ApiRoutes.cs
@@ -0,0 +1,26 @@
+﻿namespace Biblioteca.Cliente.Api.Routes
+{
+    public static class ApiRoutes
+    {
+        public const string Root = "api";
+        public const string Version = "v1";
+        public const string Base = Root + "/" + Version;
+
+        public static class RouteProducto
+        {
+            //Para Leer
+            public const string GetAll = Base+ "/cliente/all";
+            public const string GetById = Base+ "/cliente/{id}";
+
+            //Write
+            public const string Create = Base + "/producto/create";
+            public const string Update = Base + "/producto/update";
+            public const string Delete = Base + "/producto/delete";
+
+
+
+        }
+
+
+    }
+}
